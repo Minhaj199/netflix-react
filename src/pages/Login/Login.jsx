69d019563bcdf436329767bb24144784d119fb85
Minhaj199 <150225109+Minhaj199@users.noreply.github.com>
2024-07-31 18:06:56 +0530

    login lay out add

--- src/pages/Login/Login.jsx
+++ src/pages/Login/Login.jsx
@@ -1,7 +1,49 @@
-import React from 'react'
-import './Login'
+import React,{useState} from 'react'
+import './Login.css'
+import netflix_log from '../../assets/logo.png'
 export const Login = () => {
+  const [signState,setSignState]=useState('Sign In')
+
+
+
+
   return (
-    <div className='Login'>Login</div>
-  )
+    <div className="Login">
+      <img src={netflix_log} className="login-logo" alt="" />
+      <div className="login-form">
+        <h1>{signState}</h1>
+        <form>
+          {signState === "Sign Up" ? (
+            <input type="text" placeholder="Your name" />
+          ) : (
+            <></>
+          )}
+
+          <input type="email" placeholder="Email" />
+          <input type="password" placeholder="Password" />
+          <button>{signState}</button>
+          <div className="form-helper">
+            <div className="remember-me">
+              <input type="checkbox" name="" id="" />
+              <label htmlFor="">Remember Me</label>
+            </div>
+            <p>Need Help ?</p>
+          </div>
+        </form>
+        <div className="form-switch">
+          {signState === "Sign In" ? (
+            <p>
+              New to Netflix?{" "}
+              <span onClick={() => setSignState("Sign Up")}>Sign Up Now</span>
+            </p>
+          ) : (
+            <p>
+              Already have account?{" "}
+              <span onClick={() => setSignState("Sign In")}>Sign In Now</span>
+            </p>
+          )}
+        </div>
+      </div>
+    </div>
+  );
 }
