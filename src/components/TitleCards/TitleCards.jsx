97363572884eda4bc159447348b09eb1c3f60d1f
Minhaj199 <150225109+Minhaj199@users.noreply.github.com>
2024-07-31 19:39:22 +0530

    tmdb api added

--- src/components/TitleCards/TitleCards.jsx
+++ src/components/TitleCards/TitleCards.jsx
@@ -1,17 +1,43 @@
-import React from 'react'
+import React,{useEffect, useState} from 'react'
 import './TitleCards.css'
 
-import cards_data from '../../assets/cards/Cards_data'
+
+
+
 export const TitleCards = ({title,category}) => {
+  const [apitData,setApiData]=useState([])
+  const options = {
+    method: "GET",
+    headers: {
+      accept: "application/json",
+      Authorization:
+        "Bearer eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiI4ZDJjYTQwMzI2YjViNzhhNmQ0MGYwNzJkZWEwYzliNyIsIm5iZiI6MTcyMjQzMjc3Mi4wNzM1NTUsInN1YiI6IjY2YWEzYmNlNzBiZGU0NzkyMGExMGRjMSIsInNjb3BlcyI6WyJhcGlfcmVhZCJdLCJ2ZXJzaW9uIjoxfQ.hhln4GrIfQ6z1abks-stR5bzjyvvwvY_IawqnfRLV34",
+    },
+  };
+
+  
+ useEffect(()=>{
+  fetch(
+    `https://api.themoviedb.org/3/movie/${category?category:'now_playing'}?language=en-US&page=1`,
+    options
+  )
+    .then((response) => response.json())
+    .then((response) => setApiData(response.results))
+    .catch((err) => console.error(err));
+ },[])
+ 
+ 
   return (  
     <div className='title-cards'>
       <h2>{title?title:'Popular on Netflix'}</h2>
       <div className='card-list'>
-        {cards_data.map((card,index)=>{
-          return <div className="card" key={index}>
-            <img src={card.image} alt="" />
-            <p>{card.name}</p>
-          </div>
+        {apitData.map((card,index)=>{
+          return (
+            <div className="card" key={index}>
+              <img src={`https://image.tmdb.org/t/p/w500`+card.backdrop_path} alt="" />
+              <p>{card.original_title}</p>
+            </div>
+          );
         })}
       </div>
     </div>
